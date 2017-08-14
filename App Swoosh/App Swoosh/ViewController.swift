a8e83ed8023313ac591739e55661c5d3f73a5c70
Andy Taylor <Theador0691@gmail.com>
2017-08-14 09:05:44 +0100

    Initial commit and added assets

---
+++ App Swoosh/App Swoosh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App Swoosh
+//
+//  Created by Andrew Taylor on 14/08/2017.
+//  Copyright © 2017 Andrew Taylor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
