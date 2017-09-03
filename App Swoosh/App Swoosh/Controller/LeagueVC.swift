db5986a0ee6eddbbbe018904eed56446a4c91d7b
Andy Taylor <Theador0691@gmail.com>
2017-09-03 20:16:37 +0100

    added in the Player Model

--- App Swoosh/App Swoosh/Controller/LeagueVC.swift
+++ App Swoosh/App Swoosh/Controller/LeagueVC.swift
@@ -9,29 +9,37 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    
+    var player: Player!
+    
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBOutlet var nextBtn: BorderButton!
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
 
-
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        player = Player()
     }
 
     @IBAction func onNextTouch(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
-    
     }
-
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
     }
-    */
 
 }
+
+
---
+++ App Swoosh/App Swoosh/Model/Player.swift
@@ -0,0 +1,16 @@
+//
+//  Player.swift
+//  App Swoosh
+//
+//  Created by Andrew Taylor on 03/09/2017.
+//  Copyright © 2017 Andrew Taylor. All rights reserved.
+//
+
+import Foundation
+
+struct Player{
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+    
+    
+}
